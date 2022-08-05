58de40af285257ddfd8ebf2375515d936c7d4e54
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2022-08-05 11:06:57 +0200

    Add causes page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/index.html')
+
+
+def causes(request):
+    return render(request, 'pages/causes.html')
